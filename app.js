ff9497533c76064855a9ee3d9e4b184f409be6a9
FahadZulfi14 <122536758+FahadZulfi14@users.noreply.github.com>
2023-01-25 21:08:47 +0500

    Add files via upload
    this file is assignment for Js

---
+++ app.js
@@ -0,0 +1,31 @@
+
+// ____________________chapter one Alert_____________________________
+
+// Question no 1
+// alert("Helo Sir");
+
+
+
+// Question no 2
+// alert("Error! Please enter a valid password.");
+
+
+
+// Question no 3
+// alert("Welcome to JS Land...\nHappy coding!");
+
+
+
+// Question no 4
+// alert("Wellcome to JS land...")
+// alert("Happy coding!\n Prevent this page from creating additional dialogs.");
+
+
+
+// Question no 5
+// alert ("Hello... i can run JS through my web browser console");
+
+
+
+// Question no 6
+// i made alert Massage in my Html test project html file
